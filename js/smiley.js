962c7d5e4d1e75fc7a3b587e67f7d2f64872f0fc
Leonard Michiels <leonard.michiels@heig-vd.ch>
2020-02-21 15:12:50 +0100

    21022020:1512

--- js/smiley.js
+++ js/smiley.js
@@ -7,3 +7,23 @@
       bckgrd.setAttribute('fill', isBlue ? 'blue' : 'red')
     })
     
+
+    // content d="M150,220 Q128,250 230,225"
+    const smile = document.getElementById('smile')
+    const text =document.getElementById('text')
+    let sad="M150,230 Q128,128 230,225"
+    let happy="M150,220 Q128,250 230,225"
+    let isHappy=false
+     face.addEventListener('mouseover', () => {
+      isHappy = true
+
+      smile.setAttribute('d',happy)
+      text.innerHTML="Me gusta mucho !"
+    })
+ 
+    face.addEventListener('mouseout', () => {
+      isHappy = false
+
+      smile.setAttribute('d',sad)
+      text.innerHTML="No me Gusta!"
+    })
