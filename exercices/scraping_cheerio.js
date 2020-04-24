45521e7abd96a69e4f549301bc3c6df41c25b937
Leonard Michiels <leonard.michiels@heig-vd.ch>
2020-04-24 11:04:53 +0200

    scraping_cheerio.js corrections

--- exercices/scraping_cheerio.js
+++ exercices/scraping_cheerio.js
@@ -6,20 +6,22 @@ const $ = cheerio.load(page)
 let result = []
 const row = $('body > div.wrapper > div.container.test-site > div > div.col-md-9 > div')
 const element = $('div', row)
-element.each((index, h4) => {
+
+
+element.each((index, div) => {
     if (index !== 0) {
         result.push({
-            nom: $('body > div.wrapper > div.container.test-site > div > div.col-md-9 > div > div:nth-child(4) > div > div.caption > h4:nth-child(2) > a', h4).attr('title'),
-            prix: $('body > div.wrapper > div.container.test-site > div > div.col-md-9 > div > div:nth-child(4) > div > div.caption > h4.pull-right.price', h4).text()
+            Article: $('div.caption > h4:nth-child(2) > a', div).text(),
+            Prix: $('div.caption > h4.pull-right.price', div).text(),
         })
     }
 })
+
 console.log(result)
 console.log(
     JSON.stringify(
         result.map(d => ({
             ...d,
-            titres: Number(d.titres)
         }))
     )
 )
